e2d4accffc95ee95c192bcaa9b19f0caf520135c
paul <pmorin35@gmail.com>
2015-07-17 13:51:27 -0700

    new controllers for Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,30 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.find(params[:id])
+  end
+
+  def update
+    @skill= Skill.find(params[:skill_id])
+    @project = @skill.projects.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project has been edited successfully, brainiacs!"
+      redirect_to skill_path(@project.skill)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Gone baby gone!"
+    redirect_to skill_path(@project.skill)
+  end
+
   private
   def project_params
     params.require(:project).permit(:app, :info)
